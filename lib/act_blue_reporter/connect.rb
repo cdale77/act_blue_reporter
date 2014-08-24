400205907a2da362be0dbf41e41669e1cc855181
Brad Johnson <cdale77@gmail.com>
2014-08-23 19:08:35 -0700

    connect

--- lib/act_blue_reporter/connect.rb
+++ lib/act_blue_reporter/connect.rb
@@ -6,5 +6,15 @@ module ActBlueReporter
 
     ACTBLUE_URI = "https://secure.actblue.com/api/2009-08"
     HEADER = { "Accept" => "application/xml" }
+
+    # all children of this class get a success and msg attribute
+    attr_accessor :success
+    attr_accessor :message
+
+    def initialize
+      @message = ""
+      @success = false
+    end
+
   end
 end
--- spec/act_blue_reporter_spec.rb
+++ spec/act_blue_reporter_spec.rb
@@ -3,6 +3,11 @@ require "spec_helper"
 describe ActBlueReporter do
 
   describe ActBlueReporter::Connect do
+
+    let(:act_blue_connect) { ActBlueReporter::Connect.new }
+    let(:request_url) { }
+    let(:request_auth) { }
+
     describe 'class constants' do
       it 'should have a constant for the ActBlue URI' do
         expect(ActBlueReporter::Connect::ACTBLUE_URI).to eql \
@@ -12,6 +17,18 @@ describe ActBlueReporter do
         expect(ActBlueReporter::Connect::HEADER).to be_an_instance_of Hash
         expect(ActBlueReporter::Connect::HEADER["Accept"]).to eql \
         "application/xml"
+      end
+    end
+
+    describe '#initialize' do
+      it 'should create an object' do
+        expect(act_blue_connect).to be_an_instance_of ActBlueReporter::Connect
+      end
+      it 'should have #success set to false' do
+        expect(act_blue_connect.success).to be_falsey
+      end
+      it 'should have #message set to an empty string' do
+        expect(act_blue_connect.message).to eql ""
       end
     end
   end
