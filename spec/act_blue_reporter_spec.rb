253e3eb392f8df9e2bc2fb8a487d4c4ee7c648f8
Brad Johnson <cdale77@gmail.com>
2014-08-23 18:16:16 -0700

    first tests

--- spec/act_blue_reporter_spec.rb
+++ spec/act_blue_reporter_spec.rb
@@ -2,4 +2,16 @@ require "spec_helper"
 
 describe ActBlueReporter do
 
+  describe ActBlueReporter::Connect do
+    describe 'class constants' do
+      it 'should have a constant for the ActBlue URI' do
+        expect(ActBlue::Connect::ACTBLUE_URI).to eql \
+          "https://secure.actblue.com/api/2009-08"
+      end
+      it 'should have a constant for the headers' do
+        expect(ActBlue::Connect::HEADER).to be_an_instance_of Hash
+        expect(ActBlue::Connect::HEADER["Accept"]).to eql "application/xml"
+      end
+    end
+  end
 end
