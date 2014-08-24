550efaa896a111daba4e8d8e3724b812534d664c
Brad Johnson <cdale77@gmail.com>
2014-08-23 19:12:08 -0700

    some comments

--- lib/act_blue_reporter/connect.rb
+++ lib/act_blue_reporter/connect.rb
@@ -1,8 +1,11 @@
+require "httparty"
+
 module ActBlueReporter
+
   # This class provides the basic request method used to communicate with
   # ActBlue.
   class Connect < ActBlueReporter::Base
-    #include HTTParty
+    include HTTParty
 
     ACTBLUE_URI = "https://secure.actblue.com/api/2009-08"
     HEADER = { "Accept" => "application/xml" }
@@ -14,6 +17,23 @@ module ActBlueReporter
     def initialize
       @message = ""
       @success = false
+    end
+
+    private
+    def make_request(request_url, authentication)
+
+      # pre-create the response as an empty string
+      response = ""
+
+      # attempt to connect to ActBlue
+      Timeout.timeout(5) do
+        response = HTTParty.get("#{ACTBLUE_URI}#{request_url}",
+                                basic_auth: authentication,
+                                headers: HEADER)
+      end
+
+      # Parsed response is nil when an incorrect entity id is used
+      response.parsed_response ? response.parsed_response : ""
     end
 
   end
